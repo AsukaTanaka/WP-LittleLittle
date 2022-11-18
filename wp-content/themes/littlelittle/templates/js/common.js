49a496af73cb6448cee1e29cef2b53236f1cc1d3
AsukaTanaka <111677080+AsukaTanaka@users.noreply.github.com>
2022-11-18 21:51:09 +0700

    Update

---
+++ wp-content/themes/littlelittle/templates/js/common.js
@@ -0,0 +1,202 @@
+/**
+ * Call Function
+ */
+const datePick = document.getElementById('js-datepick');
+const monthCalendar = document.getElementById('js-month-calendar');
+const itemCalendar = document.getElementById('js-item-calendar');
+
+const thisDate =  new Date();
+let thisMonth = thisDate.getMonth();
+let thisYear = thisDate.getFullYear();
+
+const arrayMonth = [
+    "Tháng 1", "Tháng 2", "Tháng 3", "Tháng 4", "Tháng 5", "Tháng 6", 
+    "Tháng 7", "Tháng 8", "Tháng 9", "Tháng 10", "Tháng 11", "Tháng 12",
+]
+
+const tableMonth = [
+    ["CN", "T2", "T3", "T4", "T5", "T6", "T7"]
+]
+
+/* Toggle Menu */
+function toggleMenu() {
+    const openMenu = document.querySelector('#js-sidebar-open');
+    const closeMenu = document.querySelector('#js-sidebar-close');
+    const sidebarMenu = document.querySelector('#js-sidebar');
+
+    openMenu.addEventListener("click", () => {
+        sidebarMenu.classList.toggle('active');
+    });
+
+    closeMenu.addEventListener("click", () => {
+        sidebarMenu.classList.remove('active');
+    });
+}
+
+/* Form Index */
+function formIndex() {
+    const fullname = document.querySelector('#js-form-index #fullname');
+    const phone = document.querySelector('#js-form-index #phone');
+    const email = document.querySelector('#js-form-index #email');
+    const package = document.querySelector('#js-form-index #package');
+    const amount = document.querySelector('#js-form-index #amount');
+    const datepick = document.querySelector('#js-form-index #js-datepick');
+
+    fullname.placeholder = "Họ và tên";
+    phone.placeholder = "Số điện thoại";
+    email.placeholder = "Địa chỉ email";
+    package.placeholder = "Chọn gói";
+    package.readOnly = true;
+    amount.placeholder = "Số lượng vé";
+    datepick.placeholder = "Ngày sử dụng";
+    datepick.readOnly = true;
+}
+
+function selectMultipleButton() {
+    // Package
+    const selectValue = document.querySelector('.form-index .input-text');
+    const optionsContainer = document.querySelector('.form-index .options-container');
+    const optionList = document.querySelectorAll('.form-index .option');
+    const buttonSelect = document.querySelector('.form-index .button-select');
+
+    // Date 
+    const bgDatepick = document.querySelector('.date-box .bg-datepick');
+    const buttonDate = document.querySelector('.button-date');
+
+    buttonSelect.addEventListener("click", () => {
+        optionsContainer.classList.toggle("active");
+        bgDatepick.classList.remove("active");
+    });
+
+    buttonDate.addEventListener("click", () => {
+        bgDatepick.classList.toggle("active");
+        optionsContainer.classList.remove("active");
+    });
+
+    optionList.forEach(o => {
+        o.addEventListener("click", (e)=> {
+            selectValue.value = o.querySelector(".select-box .option label").innerHTML;
+            optionsContainer.classList.remove("active");
+        }); 
+    });
+}
+
+function createCalendar() {
+    // var arrayTableMonth = document.querySelector('.table-calendar thead');
+    // for(var i = 0; i < tableMonth.length; i++) {
+    //     var tr = document.createElement('tr');
+    //     for(var j = 0; j < tableMonth[i].length; j++) {
+    //         var th = document.createElement('th');
+    //         var tn = document.createTextNode(tableMonth[i][j]);
+    //         th.appendChild(tn);
+    //         tr.appendChild(th);
+    //     }
+    //     arrayTableMonth.appendChild(tr);
+    // }
+
+    // datePick.value = `${thisDate.getDate()}/${thisMonth + 1}/${thisYear}`;
+
+    monthCalendar.innerHTML = `${arrayMonth[thisMonth]}, ${thisYear}`;
+    const dayOne = new Date(thisYear, thisMonth).getDay();
+    const monthDays = 32 - new Date(thisYear, thisMonth, 32).getDate();
+    
+    dateCount = 1;
+
+    for(let i = 0; i < 6; i++) {
+        let row = document.createElement("tr");
+        for(let j = 0; j < 7; j++) {
+            let column = document.createElement("td");
+            if(dateCount > monthDays) break;
+            else if (i === 0 && j < dayOne) {
+                let columnText = document.createTextNode("");
+                column.classList.add("get-null");
+                column.appendChild(columnText);
+                row.appendChild(column);
+            } else {
+                let columnText = document.createTextNode(dateCount);
+                column.appendChild(columnText);
+                if(dateCount === thisDate.getDate() && thisMonth === thisDate.getMonth() && thisYear === thisDate.getFullYear()) {
+                    column.classList.add("table-today");
+                }
+                column.addEventListener("click", () => {
+                    datePick.value = `${column.textContent}/${thisMonth+1}/${thisYear}`;
+                });
+                row.appendChild(column);
+                dateCount++;
+            }
+        }
+        itemCalendar.appendChild(row);
+    }
+}
+
+function preMonth() {
+    const prevMonth = document.querySelector(".bg-datepick .prev-month");
+
+    prevMonth.addEventListener("click", () => {
+        thisMonth = thisMonth - 1;
+        itemCalendar.innerHTML = "";
+        if(thisMonth < 0) {
+            thisYear = thisYear - 1;
+            thisMonth = 11;
+        }
+
+        createCalendar();
+        return thisMonth;
+    });
+}
+
+function nextMonth() {
+    const nextMonth = document.querySelector(".bg-datepick .next-month");
+
+    nextMonth.addEventListener("click", () => {
+        thisMonth = thisMonth + 1;
+        itemCalendar.innerHTML = "";
+        if(thisMonth > 11) {
+            thisYear = thisYear + 1;
+            thisMonth = 0;
+        }
+
+        createCalendar();
+        return thisMonth;
+    });
+}
+
+function formContact() {
+    const name = document.getElementById('name');
+    const email = document.getElementById('email');
+    const phone = document.getElementById('phone');
+    const address = document.getElementById('address');
+    const comment = document.getElementById('comment');
+
+    name.placeholder = "Tên";
+    email.placeholder = "Email";
+    phone.placeholder = "Số điện thoại";
+    address.placeholder = "Địa chỉ";
+    comment.placeholder = "Lời nhắn";
+}
+
+/**
+ * Show Function
+ */
+
+if(document.getElementById('js-menu')) {
+    toggleMenu();
+}
+
+if(document.getElementById('js-select-package') && document.getElementById('js-date')) {
+    selectMultipleButton();
+}
+
+if(document.getElementById('js-form-index')) {
+    formIndex();
+}
+
+if(document.getElementById('js-datepick')) {
+    createCalendar();
+    preMonth();
+    nextMonth();
+}
+
+if(document.getElementById('js-form-contact')) {
+    formContact();
+}
